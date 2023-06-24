312625ca62e0ebc5516f6bfea9d98f5f3c25b309
profBritto <linuxwindows739@gmail.com>
2023-06-25 00:33:05 +0600

    Truefalse done

---
+++ true-false.js
@@ -0,0 +1,12 @@
+const Fname =" ";
+let Gname; //undefine
+Gname=null;//null,0,"" all false
+Gname=NaN;//null,0,"" ,NaN all false
+console.log(Gname);
+
+
+if (Gname>0) {
+    console.log("true");
+} else {
+    console.log("false");
+}
